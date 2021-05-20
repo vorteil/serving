05ab9312cbaf2c0faeeb67721d2a364ff43955c4
Jens Gerke <jens.gerke@vorteil.io>
2021-05-21 09:15:40 +1000

    action test 5
    Signed-off-by: Jens Gerke <jens.gerke@vorteil.io>

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ ko apply -Rf config/core/
 
 kubectl apply -f ./third_party/contour-latest/
 
-kubectl apply -f ~/direktiv/scripts/config-deployment.yaml
+#kubectl apply -f ~/direktiv/scripts/config-deployment.yaml
 
-kubectl patch configmap/config-network \
-  --namespace knative-serving \
-  --type merge \
-  --patch '{"data":{"ingress.class":"contour.ingress.networking.knative.dev"}}'
+#kubectl patch configmap/config-network \
+#  --namespace knative-serving \
+#  --type merge \
+#  --patch '{"data":{"ingress.class":"contour.ingress.networking.knative.dev"}}'
